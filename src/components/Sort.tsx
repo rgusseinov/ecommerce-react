3df5fd337b9c916175c2f805e4b73ea99545fee6
Ruslan <r.gusseinov@mail.ru>
2022-09-04 16:53:04 +0600

    feat: finish divide into components

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,38 +1,35 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import arrowDownIcon from '../assets/img/main/product-item/arrow-down.svg';
 
 export const Sort: React.FC = () => {
   const [showSortByPrice, setShowSortByPrice] = useState<boolean>(false);
   const [showSortByParam, setShowSortByParam] = useState<boolean>(false);
-  /*
-  // Sort by params
-  document.querySelector(".sortby-params").onclick = function (e) {
-    const popupMenu = document.querySelector(".sortby-params-popup");
-    const sortBlock = document.querySelector(".sortby-params");
-    popupMenu.classList.toggle("show");
-    sortBlock.classList.toggle("selected");
-    e.preventDefault();
-  }; */
+
+  const classSortByPrice = classNames({
+    'sortby-controls__price sort-btn sortby-price': true,
+    selected: showSortByPrice
+  });
+
+  const classShowSortByPrice = classNames({
+    'sortby-price-popup price-popup': true,
+    show: showSortByPrice
+  });
+
   return (
     <div className="product__sortby-header sortby-header">
       <div className="sortby-header__block label">Фильтр</div>
       <div className="sortby-header__block sortby-controls">
         <div className="sortby-controls__label">Сортировать:</div>
         <div
-          className={`sortby-controls__price sort-btn sortby-price ${
-            showSortByPrice ? 'selected' : ''
-          }`}
+          className={classSortByPrice}
           onClick={() => setShowSortByPrice(!showSortByPrice)}
         >
           <div className="sortby-price__container">
             По цене
             <img src={arrowDownIcon} alt="По цене" />
           </div>
-          <div
-            className={`sortby-price-popup price-popup ${
-              showSortByPrice ? 'show' : ''
-            }`}
-          >
+          <div className={classShowSortByPrice}>
             <div className="sortby-price-popup__item">Сначало дорогие</div>
             <div className="sortby-price-popup__item selected">
               Сначала дешёвые
