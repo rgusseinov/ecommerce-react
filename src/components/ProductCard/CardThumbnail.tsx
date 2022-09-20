2fbd858a24a3df4cf3905c1b7a6f8e72fd1c03c4
Ruslan Gusseinov <r.gusseinov@mail.ru>
2022-09-20 23:02:48 +0600

    fix: save before pull

--- src/components/ProductCard/CardThumbnail.tsx
+++ src/components/ProductCard/CardThumbnail.tsx
@@ -4,8 +4,60 @@ import arrowRight from '../../assets/img/main/product-item/thumbnail-arrow-right
 import scooterImg1 from '../../assets/img/main/product-item/scooter-img-1.png';
 import scooterImg2 from '../../assets/img/main/product-item/scooter-img-2.png';
 import scooterImg3 from '../../assets/img/main/product-item/scooter-img-3.png';
+import { useState } from 'react';
 
 export const CardThumbnail: React.FC = () => {
+  const [prevImage, setPrevImage] = useState<number>(1);
+  const [nextImage, setNextImage] = useState<number>(2);
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  const onPrevImage = () => {
+    setCurrentImageIndex((prev) => {
+      // if (prev < 0) return;
+      return prev - 1;
+    });
+  };
+
+  const onNextImage = () => {
+    setCurrentImageIndex((prev) => prev + 1);
+  };
+
+  /*   let slidePos = 1;
+  const slides = 2;
+  document.querySelector(".arrow-left").onclick = function (e) {
+    if (slidePos < 0) return;
+    if (slidePos >= slides) slidePos = slides - 1;
+    console.log(`left`, slidePos);
+  
+    const switchImage = document
+      .querySelector('.image-switch')
+      .querySelectorAll('div.image-switch__image');
+    clearClasses(switchImage[slidePos + 1]);
+    slidePos--;
+  };
+  
+  document.querySelector(".arrow-right").onclick = function (e) {
+    if (slidePos > slides) return;
+    if (slidePos <= 0) slidePos = 1;
+    
+    console.log(`right`, slidePos);
+  
+    const switchImage = document
+      .querySelector('.image-switch')
+      .querySelectorAll('div.image-switch__image');
+  
+      switchImage[slidePos].style.opacity = "1";
+      switchImage[slidePos].style.zIndex = "-1";
+    
+    slidePos++;
+  };
+  
+  const clearClasses = (element) => {
+    element.style.opacity = "";
+    element.style.zIndex = "";
+  }
+ */
+
   return (
     <div className="product-list-item__thumbnail product-thumbnail">
       <div className="product-thumbnail__container">
@@ -35,10 +87,16 @@ export const CardThumbnail: React.FC = () => {
             </div>
           </div>
           <ul className="product-thumbnail__navs thumbnail-navs">
-            <li className="thumbnail-navs__item arrow-left">
+            <li
+              className="thumbnail-navs__item arrow-left"
+              onClick={onPrevImage}
+            >
               <img src={arrowLeft} alt="kugoo" />
             </li>
-            <li className="thumbnail-navs__item arrow-right">
+            <li
+              className="thumbnail-navs__item arrow-right"
+              onClick={onNextImage}
+            >
               <img src={arrowRight} alt="kugoo" />
             </li>
           </ul>
